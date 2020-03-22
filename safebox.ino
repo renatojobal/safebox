ac493fe443d7a988ccbec0ea05027f64e40f7e6a
renatojobal <renatojobal@gmail.com>
2020-03-22 04:32:01 -0500

    Next step, compare UID with the matriz of allowed UIDs

--- safebox.ino
+++ safebox.ino
@@ -19,71 +19,147 @@
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
 */
 /*
-	Capitulo 40 de Arduino desde cero en Español.
-	Programa que obtiene el UID de la tarjeta o llavero del kit RFID RC522, compara con
-	valores previos obtenidos de UID y permite acceso con texto en monitor serie de bienvenida
-	Cualquier otra tarjeta o llavero no listado muestra texto de No te conozco, es decir,
-	acceso denegado. Requiere instalar libreria MFRC522
+  EN
+  Program that get the UID from the card o key kepper at the kit RFID RC522. It compares
+  the previues values UID and give acces to a sasfebox, moving the servo motor that controls
+  the lock door.
 
-	Autor: bitwiseAr  
+  It let you register some UIDS to let them the access.
+
+*/
+/*
+  ES
+  Programa que obtiene el UID de la tarjeta o llavero del kit RFID RC522, compara con
+  valores previos obtenidos de UID y permite acceso a una caja fuerte, moviendo el servo
+  motor que controla la cerradura de la puerta.
+
+  Se pueden registrar varias UIDS a las que se permite el acceso.
 
 */
 
-#include <SPI.h>			// incluye libreria bus SPI
-#include <MFRC522.h>			// incluye libreria especifica para MFRC522
+#include <SPI.h>     // Include library bus SPI
+#include <MFRC522.h> // Include library for MFRC522
 
-#define RST_PIN  9			// constante para referenciar pin de reset
-#define SS_PIN  10			// constante para referenciar pin de slave select
+#define RST_PIN 9 // Constant of reset pin
+#define SS_PIN 10 // Constant of slave select pin
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);	// crea objeto mfrc522 enviando pines de slave select y reset
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create the object mfrc522
 
-byte LecturaUID[4]; 				// crea array para almacenar el UID leido
-byte Usuario1[4]= {0x90, 0x0E, 0xE4, 0xA4} ;    // UID de tarjeta leido en programa 1
-byte Usuario2[4]= {0x06, 0x76, 0x25, 0xD9} ;    // UID de llavero leido en programa 1
+/*
+Keys
+- 09 7E D7 20 (With the house keys)
+- 56 DB BD F7
 
-void setup() {
-  Serial.begin(9600);			// inicializa comunicacion por monitor serie a 9600 bps
-  SPI.begin();				// inicializa bus SPI
-  mfrc522.PCD_Init();			// inicializa modulo lector
-  Serial.println("Listo");		// Muestra texto Listo
-}
 
-void loop() {
-  if ( ! mfrc522.PICC_IsNewCardPresent())		// si no hay una tarjeta presente
-    return;						// retorna al loop esperando por una tarjeta
-  
-  if ( ! mfrc522.PICC_ReadCardSerial()) 		// si no puede obtener datos de la tarjeta
-    return;						// retorna al loop esperando por otra tarjeta
-    
-    Serial.print("UID:");				// muestra texto UID:
-    for (byte i = 0; i < mfrc522.uid.size; i++) {	// bucle recorre de a un byte por vez el UID
-      if (mfrc522.uid.uidByte[i] < 0x10){		// si el byte leido es menor a 0x10
-        Serial.print(" 0");				// imprime espacio en blanco y numero cero
-        }
-        else{						// sino
-          Serial.print(" ");				// imprime un espacio en blanco
-          }
-          Serial.print(mfrc522.uid.uidByte[i], HEX);   	// imprime el byte del UID leido en hexadecimal
-          LecturaUID[i]=mfrc522.uid.uidByte[i];   	// almacena en array el byte del UID leido      
-          }
-          
-          Serial.print("\t");   			// imprime un espacio de tabulacion             
-                    
-          if(comparaUID(LecturaUID, Usuario1))		// llama a funcion comparaUID con Usuario1
-            Serial.println("Bienvenido Usuario 1");	// si retorna verdadero muestra texto bienvenida
-          else if(comparaUID(LecturaUID, Usuario2))	// llama a funcion comparaUID con Usuario2
-            Serial.println("Bienvenido Usuario 2");	// si retorna verdadero muestra texto bienvenida
-           else						// si retorna falso
-            Serial.println("No te conozco"); 		// muestra texto equivalente a acceso denegado          
-                  
-                  mfrc522.PICC_HaltA();  		// detiene comunicacion con tarjeta                
-}
+Cards
+- 42 37 F4 1F (With the plastic)
+- 42 78 4A 34
+- DE 96 8B 21
 
-boolean comparaUID(byte lectura[],byte usuario[])	// funcion comparaUID
+*/
+
+// byte Usuario1[4] = {0x90, 0x0E, 0xE4, 0xA4}; // UID de tarjeta leido en programa 1
+// byte Usuario2[4] = {0x06, 0x76, 0x25, 0xD9}; // UID de llavero leido en programa 1
+
+byte targetUID[4]; // for saving the result of the reader
+
+byte allowedUIDs[2][4] = {
+    {0x09, 0x7E, 0xD7, 0x20},
+    {0x42, 0x37, 0xF4, 0x1F}};
+
+void setup()
 {
-  for (byte i=0; i < mfrc522.uid.size; i++){		// bucle recorre de a un byte por vez el UID
-  if(lectura[i] != usuario[i])				// si byte de UID leido es distinto a usuario
-    return(false);					// retorna falso
+  Serial.begin(9600);        // Initialice the serial port
+  SPI.begin();               // initialice bus SPI
+  mfrc522.PCD_Init();        // initialice reder module
+  Serial.println("Ready\n"); // Ok message
+}
+
+void loop()
+{
+
+  // Control sentences
+  if (canRead())
+  { // if its possible read a UID
+    Serial.print("UID Found\n");
   }
-  return(true);						// si los 4 bytes coinciden retorna verdadero
+  else
+  {
+    return;
+  }
+
+  // Getting the read and saving it on targetUID, and printing it
+  Serial.print("UID:");
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  { // bucle recorre de a un byte por vez el UID
+    if (mfrc522.uid.uidByte[i] < 0x10)
+    {
+      Serial.print(" 0");
+    }
+    else
+    {
+      Serial.print(" ");
+    }
+    Serial.print(mfrc522.uid.uidByte[i], HEX); // imprime el byte del UID leido en hexadecimal
+    targetUID[i] = mfrc522.uid.uidByte[i];     // almacena en array el byte del UID leido
+  }
+  Serial.print("\t"); //
+
+  // Comparing the targetUID with the allowedUIDs
+  if (isAnAlloweUID())
+  {
+    Serial.print("Alloew UID");
+  }
+  else
+  {
+    Serial.print("UID not allowed");
+  }
+
+  mfrc522.PICC_HaltA(); // stop the comunication
+}
+
+
+
+boolean isAnAlloweUID()
+{ // Comparing the targetUID wit the allowedUIDs
+
+  // Comparing each UID allowedUID, so we need two fors, because it is 
+  // a matriz
+
+  for (byte i = 0; i < allowedUIDs.length(); i++){
+
+
+    
+  }
+
+  for (byte j = 0; i < mfrc522.uid.size; j++)
+  {                             
+
+
+
+    if (targetUID[j] != usuario[j]) 
+      return (false);  
+
+
+
+  }
+  return (true); 
+}
+
+
+
+boolean canRead()
+{
+
+  // Control sentences
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return false;
+  }
+
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return false;
+  }
+  return true;
 }
