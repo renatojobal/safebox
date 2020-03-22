852fefe693536caa010fbb4403d0fbc5e3c02daa
renatojobal <renatojobal@gmail.com>
2020-03-21 20:59:39 -0500

    Usando codigo que lee las IDs

--- safebox.ino
+++ safebox.ino
@@ -18,121 +18,72 @@
  * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
  * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
 */
+/*
+	Capitulo 40 de Arduino desde cero en Español.
+	Programa que obtiene el UID de la tarjeta o llavero del kit RFID RC522, compara con
+	valores previos obtenidos de UID y permite acceso con texto en monitor serie de bienvenida
+	Cualquier otra tarjeta o llavero no listado muestra texto de No te conozco, es decir,
+	acceso denegado. Requiere instalar libreria MFRC522
 
-#include <SPI.h>
-#include <MFRC522.h>
+	Autor: bitwiseAr  
 
-#define RST_PIN         9           // Configurable, see typical pin layout above
-#define SS_PIN          10          // Configurable, see typical pin layout above
+*/
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+#include <SPI.h>			// incluye libreria bus SPI
+#include <MFRC522.h>			// incluye libreria especifica para MFRC522
 
-//*****************************************************************************************//
+#define RST_PIN  9			// constante para referenciar pin de reset
+#define SS_PIN  10			// constante para referenciar pin de slave select
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);	// crea objeto mfrc522 enviando pines de slave select y reset
+
+byte LecturaUID[4]; 				// crea array para almacenar el UID leido
+byte Usuario1[4]= {0x90, 0x0E, 0xE4, 0xA4} ;    // UID de tarjeta leido en programa 1
+byte Usuario2[4]= {0x06, 0x76, 0x25, 0xD9} ;    // UID de llavero leido en programa 1
+
 void setup() {
-  Serial.begin(9600);                                           // Initialize serial communications with the PC
-  SPI.begin();                                                  // Init SPI bus
-  mfrc522.PCD_Init();                                              // Init MFRC522 card
-  Serial.println(F("Read personal data on a MIFARE PICC:"));    //shows in serial that it is ready to read
+  Serial.begin(9600);			// inicializa comunicacion por monitor serie a 9600 bps
+  SPI.begin();				// inicializa bus SPI
+  mfrc522.PCD_Init();			// inicializa modulo lector
+  Serial.println("Listo");		// Muestra texto Listo
 }
 
-//*****************************************************************************************//
 void loop() {
-
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
-
-  //some variables we need
-  byte block;
-  byte len;
-  MFRC522::StatusCode status;
-
-  //-------------------------------------------
-
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
-
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  Serial.println(F("**Card Detected:**"));
-
-  //-------------------------------------------
-
-  mfrc522.PICC_DumpDetailsToSerial(&(mfrc522.uid)); //dump some details about the card
-
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
-
-  //-------------------------------------------
-
-  Serial.print(F("Name: "));
-
-  byte buffer1[18];
-
-  block = 4;
-  len = 18;
-
-  //------------------------------------------- GET FIRST NAME
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 4, &key, &(mfrc522.uid)); //line 834 of MFRC522.cpp file
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  status = mfrc522.MIFARE_Read(block, buffer1, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  //PRINT FIRST NAME
-  for (uint8_t i = 0; i < 16; i++)
-  {
-    if (buffer1[i] != 32)
-    {
-      Serial.write(buffer1[i]);
-    }
-  }
-  Serial.print(" ");
-
-  //---------------------------------------- GET LAST NAME
-
-  byte buffer2[18];
-  block = 1;
-
-  status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, 1, &key, &(mfrc522.uid)); //line 834
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Authentication failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  status = mfrc522.MIFARE_Read(block, buffer2, &len);
-  if (status != MFRC522::STATUS_OK) {
-    Serial.print(F("Reading failed: "));
-    Serial.println(mfrc522.GetStatusCodeName(status));
-    return;
-  }
-
-  //PRINT LAST NAME
-  for (uint8_t i = 0; i < 16; i++) {
-    Serial.write(buffer2[i] );
-  }
-
-
-  //----------------------------------------
-
-  Serial.println(F("\n**End Reading**\n"));
-
-  delay(1000); //change value if you want to read cards faster
-
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  if ( ! mfrc522.PICC_IsNewCardPresent())		// si no hay una tarjeta presente
+    return;						// retorna al loop esperando por una tarjeta
+  
+  if ( ! mfrc522.PICC_ReadCardSerial()) 		// si no puede obtener datos de la tarjeta
+    return;						// retorna al loop esperando por otra tarjeta
+    
+    Serial.print("UID:");				// muestra texto UID:
+    for (byte i = 0; i < mfrc522.uid.size; i++) {	// bucle recorre de a un byte por vez el UID
+      if (mfrc522.uid.uidByte[i] < 0x10){		// si el byte leido es menor a 0x10
+        Serial.print(" 0");				// imprime espacio en blanco y numero cero
+        }
+        else{						// sino
+          Serial.print(" ");				// imprime un espacio en blanco
+          }
+          Serial.print(mfrc522.uid.uidByte[i], HEX);   	// imprime el byte del UID leido en hexadecimal
+          LecturaUID[i]=mfrc522.uid.uidByte[i];   	// almacena en array el byte del UID leido      
+          }
+          
+          Serial.print("\t");   			// imprime un espacio de tabulacion             
+                    
+          if(comparaUID(LecturaUID, Usuario1))		// llama a funcion comparaUID con Usuario1
+            Serial.println("Bienvenido Usuario 1");	// si retorna verdadero muestra texto bienvenida
+          else if(comparaUID(LecturaUID, Usuario2))	// llama a funcion comparaUID con Usuario2
+            Serial.println("Bienvenido Usuario 2");	// si retorna verdadero muestra texto bienvenida
+           else						// si retorna falso
+            Serial.println("No te conozco"); 		// muestra texto equivalente a acceso denegado          
+                  
+                  mfrc522.PICC_HaltA();  		// detiene comunicacion con tarjeta                
 }
-//*****************************************************************************************//
+
+boolean comparaUID(byte lectura[],byte usuario[])	// funcion comparaUID
+{
+  for (byte i=0; i < mfrc522.uid.size; i++){		// bucle recorre de a un byte por vez el UID
+  if(lectura[i] != usuario[i])				// si byte de UID leido es distinto a usuario
+    return(false);					// retorna falso
+  }
+  return(true);						// si los 4 bytes coinciden retorna verdadero
+}
