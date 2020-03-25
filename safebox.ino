8a3a17ba785e7a99514fdcf96050c9402ba851ed
renatojobal <renatojobal@gmail.com>
2020-03-24 20:29:28 -0500

    Adding documentation

--- safebox.ino
+++ safebox.ino
@@ -1,39 +1,56 @@
 /*
- * Initial Author: ryand1011 (https://github.com/ryand1011)
+ * Safebox
+ * Author: renatojobal (https://github.com/renatojobal)
+ * 
+ * 
+ * Program that get the UID from the card o key kepper at the kit RFID RC522. It compares
+ * the previues values UID and give acces to a sasfebox, moving the servo motor that controls
+ * the lock door.
  *
- * Reads data written by a program such as "rfid_write_personal_data.ino"
+ * It let you register some UIDS to let them the access.
  *
- * See: https://github.com/miguelbalboa/rfid/tree/master/examples/rfid_write_personal_data
- *
+ * For register the UIDS use the 'allowedUIDs' array
+
+ * 
  * Uses MIFARE RFID card using RFID-RC522 reader
  * Uses MFRC522 - Library
- * -----------------------------------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
- *             Reader/PCD   Uno/101       Mega      Nano v3    Leonardo/Micro   Pro Micro
- * Signal      Pin          Pin           Pin       Pin        Pin              Pin
- * -----------------------------------------------------------------------------------------
- * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
- * SPI SS      SDA(SS)      10            53        D10        10               10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
- * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
- * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
+ * 
+ * Connections
+ * 
+ * ---------------------------------------------------
+ *             MFRC522      Arduino       Arduino  
+ *             Reader/PCD   Uno/101       Nano v3    
+ * Signal      Pin          Pin           Pin  
+ * ---------------------------------------------------
+ * RST/Reset   RST          9             D9   
+ * SPI SS      SDA(SS)      10            D10 
+ * SPI MOSI    MOSI         11 / ICSP-4   D11 
+ * SPI MISO    MISO         12 / ICSP-1   D12 
+ * SPI SCK     SCK          13 / ICSP-3   D13 
+ * 
+ * 
+ *
+ * ---------------------------------------------------
+ * Red Led                  3             D3
+ * Green Led                8             D8
+ * Servo motor              7             D7
+ * Button                   2             D2            
+
+
+ 
+
 */
+
 /*
-  EN
-  Program that get the UID from the card o key kepper at the kit RFID RC522. It compares
-  the previues values UID and give acces to a sasfebox, moving the servo motor that controls
-  the lock door.
+  * Spanish 
+  * Programa que obtiene el UID de la tarjeta o llavero del kit RFID RC522, compara con
+  * valores previos obtenidos de UID y permite acceso a una caja fuerte, moviendo el servo
+  * motor que controla la cerradura de la puerta.
 
-  It let you register some UIDS to let them the access.
+  * Se pueden registrar varias UIDS a las que se permite el acceso.
 
-*/
-/*
-  ES
-  Programa que obtiene el UID de la tarjeta o llavero del kit RFID RC522, compara con
-  valores previos obtenidos de UID y permite acceso a una caja fuerte, moviendo el servo
-  motor que controla la cerradura de la puerta.
-
-  Se pueden registrar varias UIDS a las que se permite el acceso.
+  * Para registrar las UIDS usa el arreglo allowedUIDs
 
 */
 
@@ -44,59 +61,56 @@
 #define RST_PIN 9 // Constant of reset pin
 #define SS_PIN 10 // Constant of slave select pin
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create the object mfrc522
-
-/*
-Keys
-- 09 7E D7 20 (With the house keys)
-- 56 DB BD F7
-
-
-Cards
-- 42 37 F4 1F (With the plastic)
-- 42 78 4A 34
-- DE 96 8B 21
-
-*/
-
-// byte Usuario1[4] = {0x90, 0x0E, 0xE4, 0xA4}; // UID de tarjeta leido en programa 1
-// byte Usuario2[4] = {0x06, 0x76, 0x25, 0xD9}; // UID de llavero leido en programa 1
-
-byte targetUID[4]; // for saving the result of the reader
-
+// Put here your UIDS  of the Cards.
+// Remember to change the numbere of de first '[]', for example if you have only one UID, you
+// should put like this 'byte allowedUIDs[1][4] = { {...} }'
 byte allowedUIDs[2][4] = {
     {0x09, 0x7E, 0xD7, 0x20},
     {0x42, 0x37, 0xF4, 0x1F}};
 
+byte targetUID[4]; // for saving the result of the reader
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create the object mfrc522
+
+// Ataching pins
 int greenLedPin = 8;
 int redLedPin = 3;
-Servo servomotor;
 int servoPin = 7;
 int minPulse = 700;
 int maxPulse = 2300;
 int buttonPin = 2;
 
+Servo servomotor; // Creating a instance of Servo
+
+/* 
+  This function executes only one time, when the Arduino is connected to power.
+*/
 void setup()
 {
   Serial.begin(9600); // Initialice the serial port
   SPI.begin();        // initialice bus SPI
   mfrc522.PCD_Init(); // initialice reder module
 
+  // Ataching some
   pinMode(greenLedPin, OUTPUT);
   pinMode(redLedPin, OUTPUT);
   pinMode(greenLedPin, INPUT);
+
   servomotor.attach(servoPin);
   closeDoor();
+
   Serial.println("Ready\n"); // Ok message
 }
 
+/* 
+  Principal function. It will be executed forever. After the setup function.
+*/
 void loop()
 {
   delay(200);
 
   if (buttonPressed())
   {
-
     closeDoor();
   }
 
@@ -113,7 +127,7 @@ void loop()
   // Getting the read and saving it on targetUID, and printing it
   Serial.print("UID:");
   for (byte i = 0; i < mfrc522.uid.size; i++)
-  { // bucle recorre de a un byte por vez el UID
+  {
     if (mfrc522.uid.uidByte[i] < 0x10)
     {
       Serial.print(" 0");
@@ -122,10 +136,10 @@ void loop()
     {
       Serial.print(" ");
     }
-    Serial.print(mfrc522.uid.uidByte[i], HEX); // imprime el byte del UID leido en hexadecimal
-    targetUID[i] = mfrc522.uid.uidByte[i];     // almacena en array el byte del UID leido
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    targetUID[i] = mfrc522.uid.uidByte[i];
   }
-  Serial.print("\t"); //
+  Serial.print("\t");
 
   // Comparing the targetUID with the allowedUIDs
   if (isAnAlloweUID())
@@ -143,6 +157,9 @@ void loop()
   mfrc522.PICC_HaltA(); // stop the comunication
 }
 
+/*
+  It returns true when the read UID is in the 'allowedUIDs' array.
+*/
 boolean isAnAlloweUID()
 { // Comparing the targetUID wit the allowedUIDs
 
@@ -160,6 +177,9 @@ boolean isAnAlloweUID()
   return (false);
 }
 
+/*
+  Returns true when the args passed are equals.
+*/
 boolean areSameUIDS(byte target1[], byte target2[])
 {
   for (byte i = 0; i < mfrc522.uid.size; i++)
@@ -170,10 +190,12 @@ boolean areSameUIDS(byte target1[], byte target2[])
   return (true);
 }
 
+/*
+  Control sentences
+*/
 boolean canRead()
 {
 
-  // Control sentences
   if (!mfrc522.PICC_IsNewCardPresent())
   {
     return false;
@@ -187,15 +209,9 @@ boolean canRead()
 }
 
 /*
-handleCorrectUID
-
-The digital pins allowed are: 8, 7, 6, 5, 4, 3, 2
-The analog  pins allowed are: 1, 2, 3, 4, 5, 6, 7
-
-
-
+ This function is executed when the UID of the card read its allowe.
+ We open the door.
 */
-
 void handleCorrectUID()
 {
   digitalWrite(greenLedPin, HIGH);
@@ -205,22 +221,29 @@ void handleCorrectUID()
 }
 
 /*
-handleinCorrectUID
-
-
+  This function is executed when the UID of the car read is not allowed.
+  We turn on the red led to show the user that we read the UID but the it is not in the allowed UID list. 
 */
 void handleIncorrectUID()
 {
+
   digitalWrite(redLedPin, HIGH);
   delay(2000);
   digitalWrite(redLedPin, LOW);
 }
 
+/*
+  To open the door with the servo motor
+*/
 void openDoor()
 {
   servomotor.write(180);
   delay(200);
 }
+
+/*
+  To close the door with the servo motor
+*/
 void closeDoor()
 {
   servomotor.write(0);
@@ -229,6 +252,9 @@ void closeDoor()
   digitalWrite(redLedPin, LOW);
 }
 
+/*
+  Listenner of the button
+*/
 boolean buttonPressed()
 {
   Serial.print(digitalRead(buttonPin));
